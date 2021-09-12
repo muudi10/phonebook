8afbbc0fdd2563cc3fef7163fb1abf66b51d653b
mahmoud jeilani <jeilani@gmail.com>
2021-09-12 23:57:19 +0100

    changede

--- config/database.js
+++ config/database.js
@@ -19,4 +19,5 @@ module.exports = new Sequelize('phonebookapp', 'b8d79f1daeb050', '5b61b519', {
   host: 'us-cdbr-east-04.cleardb.com',
   port: 3306,
   dialect: "mysql",
+  database:heroku_79fb0617e037b18
 });
