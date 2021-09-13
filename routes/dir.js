9a7ace914020343bd0f5a1d9f28f380d9b9dc06c
mahmoud jeilani <jeilani@gmail.com>
2021-09-13 17:59:37 +0100

    revised

--- routes/dir.js
+++ routes/dir.js
@@ -27,10 +27,10 @@ router.get("/", (req, res) =>
 router.get("/add", (req, res) => res.render("add"));
 
 //Add contact details
-router.post = async("/add", (req, res) => {
+router.post("/add", (req, res) => {
   const { first_name, last_name, contact_email, telephone, pager, department } =
     req.body;
- await  Directory.create({
+  Directory.create({
     first_name: first_name,
     last_name: last_name,
     telephone: telephone,
@@ -38,8 +38,8 @@ router.post = async("/add", (req, res) => {
     contact_email: contact_email,
     department: department,
   })
-await res.render('success')
-await res.redirect('/')
+    .catch((err) => console.log(err))
+    .then(res.render("success"));
 });
 
 // Add search ,
