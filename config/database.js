bbc917e2e5a697c639772d05370fbb18b03d537c
mahmoud jeilani <jeilani@gmail.com>
2021-09-17 23:53:26 +0100

    aws logic working

--- config/database.js
+++ config/database.js
@@ -2,16 +2,6 @@ const Sequelize = require("sequelize");
 require('dotenv').config()
 const path = require("path");
 
-// // extract any command line arguments from argv
-// const args = process.argv.slice(2)[0];
-
-// // use args to determine if .env or .env.test should be loaded
-// const envFile = args === "test" ? "../.env" : "../.env.test";
-
-// // load environment variables from env files
-// require("dotenv").config({
-//   path: path.join(__dirname, envFile),
-// });
 
 
 
@@ -23,10 +13,3 @@ module.exports = new Sequelize( 'phonebook', 'root','password', {
   dialect: "mysql",
 });
 
-// // const { DB_USER, DB_NAME, PASSWORD, PORT, HOST } = process.env;
-
-// module.exports = new Sequelize('heroku_79fb0617e037b18', 'b8d79f1daeb050', '5b61b519', {
-//   host: 'us-cdbr-east-04.cleardb.com',
-//   port: 3306,
-//   dialect: "mysql",
-// });
